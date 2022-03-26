ca6839bca12e630806a5945c9afd6cd974bd5db8
Febby Azis <febbyazis02@gmail.com>
2022-03-27 05:03:53 +0700

    commit

---
+++ Pertemuan2/tampil_data.php
@@ -0,0 +1,124 @@
+<?php 	
+include('koneksi.php');
+$db = new database();
+$data_hewan = $db->tampil_data();
+$data_hewan_penjualan = $db->tampil_data_hewan();
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+        h1 {
+            text-align: center;
+        }	
+
+        table, 
+        td,
+        th {
+        border: 1px solid #ddd; 
+        text-align: left;
+        }
+
+        table {
+        border-collapse: collapse; 
+        width: 100%;
+        background-color: blanchedalmond;
+        }
+
+
+        footer{
+            text-align: center;
+            font-size: 10pt;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        th,
+        td {
+        padding: 15px;
+        }
+
+        .col-2{
+            padding: 20px;
+        }
+
+    </style>
+</head>
+<body>
+<div class="col-2">
+    <h1>Detail Data Hewan</h1>
+    <a href="index.php">Home</a>
+    <br> <br>
+    <a href="tambah_data.php">Tambah Data Hewan</a>
+    <br> <br>
+    <a href="print.php" target="_blank">Cetak Data</a>
+    </div>
+<table border="1";>
+		<tr>
+			<th>No</th>
+			<th>Nama Hewan</th>
+			<th>Jenis Hewan</th>
+			<th>Jenis Kelamin</th>
+			<th>Warna</th>
+			<th>Harga</th>
+            <th>Action</th>
+		</tr>
+		<?php 
+		$no = 1;
+		foreach($data_hewan as $row){
+			?>
+			<tr>
+				<td><?php echo $no++; ?></td>
+				<td><?php echo $row['nama_hewan']; ?></td>
+				<td><?php echo $row['jenis_hewan']; ?></td>
+				<td><?php echo $row['jenis_kelamin']; ?></td>
+				<td><?php echo $row['warna']; ?></td>
+                <td><?php echo $row['harga']; ?></td>
+				<td>
+					<a href="edit.php?id=<?php echo $row['id_hewan']; ?>">Update</a> /
+					<a href="proses_data.php?action=delete&id=<?php echo $row['id_hewan']; ?>">Delete</a>
+				</td>
+			</tr>
+			<?php 
+		}
+		?>
+	</table>
+    <br>
+    <hr>
+    <br>
+<div class="col-2">
+    <h1>Detail Pesanan Pelanggan</h1>
+    <a href="print_penjualan.php" target="_blank">Cetak Data Pesanan Pelanggan</a>
+    </div>
+    <table border="1";>
+		<tr>
+			<th>No</th>
+			<th>Nama Hewan</th>
+			<th>Jenis Hewan</th>
+			<th>Jenis Kelamin</th>
+			<th>Warna</th>
+			<th>Harga</th>
+		</tr>
+		<?php 
+		$no = 1;
+		foreach($data_hewan_penjualan as $row){
+			?>
+			<tr>
+				<td><?php echo $no++; ?></td>
+				<td><?php echo $row['nama_hewan_penjualan']; ?></td>
+				<td><?php echo $row['jenis_hewan_penjualan']; ?></td>
+				<td><?php echo $row['jenis_kelamin_penjualan']; ?></td>
+				<td><?php echo $row['warna_penjualan']; ?></td>
+                <td><?php echo $row['harga_penjualan']; ?></td>
+			</tr>
+			<?php 
+		}
+		?>
+	</table>
+    <br> <br>
+
+    <footer>CIREBON PET SHOP</footer>
+</body>
+</html>
