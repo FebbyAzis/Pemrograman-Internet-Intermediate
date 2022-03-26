992477fab9f75674611aefe29274b9fc3f83aed6
Febby Azis <febbyazis02@gmail.com>
2022-03-27 05:01:56 +0700

    commit

---
+++ Pertemuan2/koneksi.php
@@ -0,0 +1,55 @@
+<?php 
+class database{
+ 
+	var $host = "localhost";
+	var $username = "root";
+	var $password = "";
+	var $database = "pet_shop";
+	var $koneksi = "";
+	function __construct(){
+		$this->koneksi = mysqli_connect($this->host, $this->username, $this->password,$this->database);
+		if (mysqli_connect_errno()){
+			echo "Koneksi database gagal : " . mysqli_connect_error();
+		}
+	}
+ 
+	function tampil_data_hewan()
+	{
+		$data = mysqli_query($this->koneksi,"select * from tb_penjualan");
+		while($row = mysqli_fetch_array($data)){
+			$hasil[] = $row;
+		}
+		return $hasil;
+	}
+
+	function tampil_data()
+	{
+		$data = mysqli_query($this->koneksi,"select * from tb_hewan");
+		while($row = mysqli_fetch_array($data)){
+			$hasil[] = $row;
+		}
+		return $hasil;
+	}
+ 
+	function tambah_data($nama_hewan,$jenis_hewan,$jenis_kelamin,$warna,$harga)
+	{
+		mysqli_query($this->koneksi,"insert into tb_hewan values ('','$nama_hewan','$jenis_hewan','$jenis_kelamin','$warna','$harga')");
+	}
+ 
+	function get_by_id($id_hewan)
+	{
+		$query = mysqli_query($this->koneksi,"select * from tb_hewan where id_hewan='$id_hewan'");
+		return $query->fetch_array();
+	}
+ 
+	function update_data($nama_hewan,$jenis_hewan,$jenis_kelamin,$warna,$harga,$id_hewan)
+	{
+		$query = mysqli_query($this->koneksi,"update tb_hewan set nama_hewan='$nama_hewan',jenis_hewan='$jenis_hewan',jenis_kelamin='$jenis_kelamin',warna='$warna',harga='$harga' where id_hewan='$id_hewan'");
+	}
+ 
+	function delete_data($id_hewan)
+	{
+		$query = mysqli_query($this->koneksi,"delete from tb_hewan where id_hewan='$id_hewan'");
+	}
+}
+?>
---
+++ Pertemuan2/print_penjualan.php
@@ -0,0 +1,73 @@
+<?php 	
+include('koneksi.php');
+$db = new database();
+$data_hewan_penjualan = $db->tampil_data_hewan();
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Cetak Data</title>
+    <style>
+        h1 {
+            text-align: center;
+            font-size: 18pt;
+        }	
+
+        table, 
+        td,
+        th {
+        border: 1px solid #ddd; 
+        text-align: left;
+        }
+
+        table {
+        border-collapse: collapse; 
+        width: 100%;
+        }
+
+        th,
+        td {
+        padding: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Laporan Data Pesanan Pelanggan ┃ CIREBON PET SHOP</h1>
+    <table>
+        <thead>
+            <tr>
+            <th>No</th>
+			<th>Nama Hewan</th>
+			<th>Jenis Hewan</th>
+			<th>Jenis Kelamin</th>
+			<th>Warna</th>
+            <th>Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php 
+		$no = 1;
+		foreach($data_hewan_penjualan as $row){
+			?>
+			<tr>
+				<td><?php echo $no++; ?></td>
+				<td><?php echo $row['nama_hewan_penjualan']; ?></td>
+				<td><?php echo $row['jenis_hewan_penjualan']; ?></td>
+				<td><?php echo $row['jenis_kelamin_penjualan']; ?></td>
+				<td><?php echo $row['warna_penjualan']; ?></td>
+                <td><?php echo $row['harga_penjualan']; ?></td>
+			</tr>
+			<?php 
+		}
+		?>
+        </tbody>
+    </table>
+    <script>
+         window.print();
+    </script>
+    </body>
+    
+    </html>
+    
